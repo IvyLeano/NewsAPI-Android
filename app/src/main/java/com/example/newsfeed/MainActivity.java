1889ecd6a180477810a0de6f33cbb36c64e20f42
unknown <IvyLeano@outlook.com>
2019-10-25 11:49:01 -0400

    imported bundle

---
+++ app/src/main/java/com/example/newsfeed/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.newsfeed;
+import android.os.Bundle;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.toolbox.Volley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+    }
+}
