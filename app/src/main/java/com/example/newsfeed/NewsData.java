80f83bf76dbc909889202539142ec71f7e1cb4df
unknown <IvyLeano@outlook.com>
2019-10-25 17:16:10 -0400

    fixed setter function in NewsData

--- app/src/main/java/com/example/newsfeed/NewsData.java
+++ app/src/main/java/com/example/newsfeed/NewsData.java
@@ -9,10 +9,42 @@ public class NewsData {
         private String url;
         private String category;
         private String language;
-        private String en;
         private String country;
 
-    public void setData(JSONObject object){
-        System.out.println("in setdata(): " + object + "************************");
+        //Setter method
+        public void setData(JSONObject object){
+            try {
+                this.id = object.getString("id");
+                this.name = object.getString("name");
+                this.description = object.getString("description");
+                this.url = object.getString("url");
+                this.category = object.getString("category");
+                this.language = object.getString("language");
+                this.category = object.getString("country");
+            } catch(Exception e) {
+                System.out.println("Logged from setData() in NewsData.java " + e);
+            }
+        }
+        //Getter methods
+        public String getId(){
+            return this.id;
+        }
+        public String getName(){
+            return this.name;
+        }
+        public String getDescription(){
+            return this.description;
+        }
+        public String getUrl(){
+            return this.url;
+        }
+        public String getCategory(){
+            return this.category;
+        }
+        public String getLanguage(){
+            return this.language;
+        }
+        public String getCountry(){
+            return this.country;
+        }
     }
-}
--- app/src/main/java/com/example/newsfeed/NewsRestApi.java
+++ app/src/main/java/com/example/newsfeed/NewsRestApi.java
@@ -1,18 +1,20 @@
 package com.example.newsfeed;
-
+import java.util.Vector;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class NewsRestApi {
 
+    NewsData[] newsData;
 
     public JsonObjectRequest getNews(){
         String baseUrl = "https://newsapi.org/v2/sources?language=en&apiKey=";
         String api = "ea59d14adf274f3294daa1fbae58ae92";
-        final NewsData newsData = new NewsData();
 
         // 2. construct request
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
@@ -21,9 +23,11 @@ public class NewsRestApi {
                 Request.Method.GET, baseUrl + api, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-//                System.out.println(response);
-                newsData.setData(response);
-
+                try {
+                    populateNewsDataArray(response.getJSONArray("sources"));
+                } catch (Exception e){
+                   System.out.println("Logged from JsonObjectRequest() in NewsRestApi.java: " + e);
+                }
             }
         }, new Response.ErrorListener() {
             @Override
@@ -33,5 +37,23 @@ public class NewsRestApi {
         }
         );
       return jsonObjectRequest;
+    }
+
+    public void populateNewsDataArray(JSONArray objects){
+        for(int i = 0; i < objects.length(); i++){
+            NewsData newsData = new NewsData();
+            try {
+                newsData.setData(objects.getJSONObject(i));
+                this.newsData[i] = newsData;
+            } catch (Exception e){
+                System.out.println("Logged from populateNewsDataArray() in NewsRestApi.java: " + e);
+            }
+        }
+    }
+
+    public void getNewsDataArray(){
+        for(int i = 0; i < newsData.length; i++){
+            System.out.println("************ id: " + newsData[i].getId());
+        }
     }
 }
