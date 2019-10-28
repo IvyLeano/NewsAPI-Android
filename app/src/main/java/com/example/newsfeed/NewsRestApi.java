251cde7384adcc2cf3c75169e47d3cedcb555821
unknown <IvyLeano@outlook.com>
2019-10-28 16:46:28 -0400

    moved some logic to main

--- app/src/main/java/com/example/newsfeed/NewsRestApi.java
+++ app/src/main/java/com/example/newsfeed/NewsRestApi.java
@@ -1,4 +1,5 @@
 package com.example.newsfeed;
+import java.io.Serializable;
 import java.util.Vector;
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -8,52 +9,11 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class NewsRestApi {
+public class NewsRestApi implements Serializable {
 
-    Vector newsData = new Vector();
 
-    public JsonObjectRequest getNews(){
 
-        String baseUrl = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=";
-        String api = "ea59d14adf274f3294daa1fbae58ae92";
 
-        // 2. construct request
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                //method, endpoint (where the data is coming from), input parameter object type,
-                //callback method, error response
-                Request.Method.GET, baseUrl + api, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    populateNewsDataArray(response.getJSONArray("articles"));
-                } catch (Exception e){
-                   System.out.println("Logged from JsonObjectRequest() in NewsRestApi.java: " + e);
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                System.out.println(error);
-            }
-        }
-        );
-      return jsonObjectRequest;
-    }
 
-    public void populateNewsDataArray(JSONArray objects){
-        for(int i = 0; i < objects.length(); i++){
-            try {
-                NewsData newsData = new NewsData();
-                newsData.setData(objects.getJSONObject(i));
-                this.newsData.add(newsData);
-            } catch (Exception e){
-                System.out.println("Logged from populateNewsDataArray() in NewsRestApi.java: " + e);
-            }
-        }
-        getNewsDataArray();
-    }
 
-    public Vector getNewsDataArray(){
-        return this.newsData;
-    }
 }
--- app/src/main/java/com/example/newsfeed/ScrollingActivity.java
+++ app/src/main/java/com/example/newsfeed/ScrollingActivity.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,11 +23,18 @@ import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    public String baseUrl = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=";
+    public String api = "ea59d14adf274f3294daa1fbae58ae92";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,31 +46,50 @@ public class ScrollingActivity extends AppCompatActivity {
         // 1. create a request queue
         RequestQueue requestQueue = Volley.newRequestQueue(this);
 
-        // 2. construct request - code found in newsRestApi.getNews()
-        NewsRestApi newsRestApi = new NewsRestApi();
-        JsonObjectRequest jsonObjectRequest = newsRestApi.getNews();
+        // 2. construct request
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                //method, endpoint (where the data is coming from), input parameter object type,
+                //callback method, error response
+                Request.Method.GET, baseUrl + api, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    // Store response to a vector array
+                    Vector newsData = new Vector();
+                    JSONArray array = response.getJSONArray("articles");
 
+                    for(int i = 0; i < array.length(); i++){
+                        try {
+                            NewsData news = new NewsData();
+                            news.setData(array.getJSONObject(i));
+                            newsData.add(news);
+                        } catch (Exception e){
+                            System.out.println("Logged from populateNewsDataArray() in NewsRestApi.java: " + e);
+                        }
+                    }
+                    setView(newsData);
+                } catch (Exception e){
+                    System.out.println("Logged from JsonObjectRequest() in NewsRestApi.java: " + e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error);
+            }
+        }
+        );
         // 3. add request to request queue
         requestQueue.add(jsonObjectRequest);
+    }
+    public void setView(Vector data){
 
-        // Creating Views
-//        for(int i = 0; i < newsRestApi.getNewsDataArray().size(); i++) {
-//
-//            NewsData newsData = (NewsData)newsRestApi.getNewsDataArray().get(i);
-//            System.out.println("!!!!!!!!!!!!!!!! " + newsData.);
-//
-//            // 1. create a LinearLayout
-//            LinearLayout layout = new LinearLayout(this);
-//
-//            // 2. create a TextView for the heading
-//            TextView heading = new TextView(this);
-//            heading.setText(newsData.getTitle());
-//
-//            // 3. add TextView to LinearLayout
-//            layout.addView(heading);
-//
-//            // 4. add LinearLayout to ContentView
-//            setContentView(layout);
-//        }
+
     }
 }
+
+
+
+
+
+
