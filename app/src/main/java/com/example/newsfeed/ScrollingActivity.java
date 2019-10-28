e9dce6c613aba6dfd8f5514c2a09d7f095b15bc1
unknown <IvyLeano@outlook.com>
2019-10-28 16:51:29 -0400

    main cleanup

--- app/src/main/java/com/example/newsfeed/ScrollingActivity.java
+++ app/src/main/java/com/example/newsfeed/ScrollingActivity.java
@@ -1,33 +1,15 @@
 package com.example.newsfeed;
-
-import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Vector;
 
 public class ScrollingActivity extends AppCompatActivity {
@@ -48,24 +30,18 @@ public class ScrollingActivity extends AppCompatActivity {
 
         // 2. construct request
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                //method, endpoint (where the data is coming from), input parameter object type,
-                //callback method, error response
                 Request.Method.GET, baseUrl + api, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    // Store response to a vector array
+                    // 2.a. response logic - store response to a vector array
                     Vector newsData = new Vector();
                     JSONArray array = response.getJSONArray("articles");
 
                     for(int i = 0; i < array.length(); i++){
-                        try {
                             NewsData news = new NewsData();
                             news.setData(array.getJSONObject(i));
                             newsData.add(news);
-                        } catch (Exception e){
-                            System.out.println("Logged from populateNewsDataArray() in NewsRestApi.java: " + e);
-                        }
                     }
                     setView(newsData);
                 } catch (Exception e){
@@ -77,8 +53,7 @@ public class ScrollingActivity extends AppCompatActivity {
             public void onErrorResponse(VolleyError error) {
                 System.out.println(error);
             }
-        }
-        );
+        });
         // 3. add request to request queue
         requestQueue.add(jsonObjectRequest);
     }
